4df9b6653af717e295ab67f34eb79541b9145636
buituanson85 <65030430+buituanson85@users.noreply.github.com>
2020-11-08 19:56:50 +0700

    one

---
+++ src/Exception/Exception.java
@@ -0,0 +1,7 @@
+package Exception;
+
+public class Exception {
+    /*exception
+    * try catsh finally custom exception
+    * debug trong quá trình phát triển dự án package -> java.util*/
+}
---
+++ src/Java2/lession1/Main.java
@@ -0,0 +1,7 @@
+package Java2.lession1;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
